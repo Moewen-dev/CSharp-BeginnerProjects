7d4be54a8019f51f39fa080a84f51d1edf224456
Moewen-dev <tom.penning@pm.me>
2023-10-07 20:46:32 +0200

    Started to move Methods from the Program.cs class to Logic.cs

--- Unit-Converter/Logic.cs
+++ Unit-Converter/Logic.cs
@@ -2,9 +2,63 @@
 {
     public class Logic
     {
-        public Logic() 
+        //Start Menu
+        public static void startMenu()
         {
-            
+            Console.WriteLine("Welcome to the Simple Unit Converter.");
+            Console.WriteLine("Please choose from the following options.");
+            Console.WriteLine("Do not write the Unit in the input!");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Option 01: Meter to Feet");
+            Console.WriteLine("Option 02: Meter to Inch");
+            Console.WriteLine("Option 03: Meter to Yard");
+            Console.WriteLine("Option 04: Feet to Meter");
+            Console.WriteLine("Option 05: Inch to Meter");
+            Console.WriteLine("Option 06: Yard to Meter");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Option 07: Fahrenheit to Celsius");
+            Console.WriteLine("Option 08: Celsius to Fahrenheit");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Option 09: Gram to Ounce");
+            Console.WriteLine("Option 10: Gram to Pound");
+            Console.WriteLine("Option 11: Gram to Kilogram");
+            Console.WriteLine("Option 12: Ounce to Gram");
+            Console.WriteLine("Option 13: Pound to Gram");
+            Console.WriteLine("Option 14: Kilogram to Gram");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Option 99: Exit");
+        }
+
+        //Get the Option the User wants to use
+        public static int getOption()
+        {
+            Console.WriteLine("");
+            Console.Write("Option Picked: ");
+            try
+            {
+                int option = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                return option;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return -1;
+            }
+        }
+
+        //Convert Meter to Feet
+        public static double meterToFeet(double input) { return input / 0.3048D; }
+
+        //Output Method
+        public static void output(int optionID)
+        {
+            Console.Clear();
+            switch (optionID)
+            {
+                case 99:
+                    
+            }
         }
     }
 }
--- Unit-Converter/Program.cs
+++ Unit-Converter/Program.cs
@@ -2,60 +2,15 @@
  * A simple Unit Converter, that can convert Imperial to Metric and back.
  * Made by Moewen-dev
  */
-
-
-// Start Menu:
-static void startMenu() 
-{
-    Console.WriteLine("Welcome to the Simple Unit Converter.");
-    Console.WriteLine("Please choose from the following options.");
-    Console.WriteLine("Do not write the Unit in the input!");
-    Console.WriteLine("---------------------------------------------");
-    Console.WriteLine("Option 01: Meter to Foot");
-    Console.WriteLine("Option 02: Meter to Inch");
-    Console.WriteLine("Option 03: Meter to Yard");
-    Console.WriteLine("Option 04: Foot to Meter");
-    Console.WriteLine("Option 05: Inch to Meter");
-    Console.WriteLine("Option 06: Yard to Meter");
-    Console.WriteLine("---------------------------------------------");
-    Console.WriteLine("Option 07: Fahrenheit to Celsius");
-    Console.WriteLine("Option 08: Celsius to Fahrenheit");
-    Console.WriteLine("---------------------------------------------");
-    Console.WriteLine("Option 09: Gram to Ounce");
-    Console.WriteLine("Option 10: Gram to Pound");
-    Console.WriteLine("Option 11: Gram to Kilogram");
-    Console.WriteLine("Option 12: Ounce to Gram");
-    Console.WriteLine("Option 13: Pound to Gram");
-    Console.WriteLine("Option 14: Kilogram to Gram");
-    Console.WriteLine("---------------------------------------------");
-    Console.WriteLine("Option 99: Exit");
-}
-
-static int getOption()
-{
-    try 
-    { 
-        int option =  Int32.Parse(Console.ReadLine());
-        return option;
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine("Error: " + e.ToString());
-        return -1;
-    }
-}
-
-static double meterToFoot(double input)
-{
-    return input / 0.3048D;
-}
+using Unit_Converter;
 
 while (true)
 {
     //Main Loop
     Console.Clear();
-    startMenu();
-    int option = getOption();
+    Logic.startMenu();
+    int option = Logic.getOption();
+    
     if (option == 99)
     {
         Console.Clear();
@@ -72,14 +27,21 @@ while (true)
             Console.Write("Input: ");
             double input = Double.Parse(Console.ReadLine());
             Console.Write("");
-            Console.WriteLine("Output: " + meterToFoot(input) + "Feet");
+            Console.WriteLine("Output: " + Logic.meterToFeet(input) + " feet");
             Console.WriteLine("Press Enter to Continue");
             Console.ReadKey();
         }
         catch (Exception e) 
         {
-            Console.WriteLine("Error: " + e.ToString());
+            Console.WriteLine("Error: Invalid Input!");
+            Console.WriteLine("Error: Only Numbers are Valid!");
+            Console.WriteLine("Press Enter to Return to the Menu");
             Console.ReadKey();
         }
     }
+    else if (option == 02)
+    {
+        Console.Clear();
+    }
+    
 }
