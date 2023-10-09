76d01841eed6866a876cba909a9aa4eebfde2a7f
Moewen-dev <tom.penning@pm.me>
2023-10-09 07:35:21 +0200

    Added Logic Class and created a main menu and an exit method

---
+++ CurrencyConverter/Logic.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Simple CLI Currency Converter that Supports 160 Currencies
+ * Made by Meowen-dev
+ */
+
+
+internal class Logic
+{
+    //Print Main Menu in CLI
+    public static void SetMainMenu()
+    {
+        Console.WriteLine("----------------Main-Menu----------------");
+        Console.WriteLine("Welcome to a Currency Converter");
+        Console.WriteLine("Just enter the Currency you want to Convert from \nand enter the Currency you want to Convert to in the Next Prompt!");
+        Console.WriteLine("In the Last Prompt you just put in the amount you want to Convert");
+        Console.WriteLine("Here is an Example: EUR to USD and 100");
+        Console.WriteLine("-----------------------------------------");
+    }
+
+    //Method to exit the Program
+    public static void exit () { Environment.Exit(0); }
+}
--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Simple CLI Currency Converter that Supports 160 Currencies
+ * Made by Meowen-dev
+ */
+
+//Main Loop
+while (true)
+{
+    Console.Clear();
+    Logic.SetMainMenu();
+    Console.ReadKey();
+    Logic.exit();
+}
